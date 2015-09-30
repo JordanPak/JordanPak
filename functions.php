6c438ae3958be02fe7867ca0d0a5cf279b478124
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-09-29 21:14:38 -0400

    Config fonts w/ Bebas Fallback to Roboto Condensed

--- functions.php
+++ functions.php
@@ -27,3 +27,15 @@ add_theme_support( 'genesis-responsive-viewport' );
 define( 'CHILD_THEME_NAME', 'JordanPak' );
 define( 'CHILD_THEME_URL', 'http://JordanPak.com/' );
 define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+
+
+// ENQUEUE GLOBAL STYLES
+add_action( 'wp_enqueue_scripts', 'jpak_global_styles' );
+function jpak_global_styles() {
+
+    // wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto:400,300,400italic,500,700', array(), CHILD_THEME_VERSION );
+    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto:400,300,400italic,500,700|Roboto+Condensed:400,700', array(), CHILD_THEME_VERSION );
+    wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array(), CHILD_THEME_VERSION );
+
+} // jpak_global_styles()
