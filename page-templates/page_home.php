ba0bab3d9f4004f85231a712e03228fae0d3d93a
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-10-02 00:11:27 -0400

    Init Home Page Template with home-hero Widget Area

--- page-templates/page_home.php
+++ page-templates/page_home.php
@@ -8,3 +8,29 @@
  * @package JordanPak
  * @since 1.0.0 (when the file was introduced)
  */
+
+ /*
+  * Template Name: Home
+  */
+
+
+ // REMOVE SIDEBAR & POST CONTENT //
+ remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+ //remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+ remove_action( 'genesis_loop', 'genesis_do_loop' );
+
+
+
+
+ // HOME HERO WIDGET AREA //
+ add_action( 'genesis_before_content', 'jpak_home_hero' );
+ function jpak_home_hero() {
+
+     // Home Hero
+     genesis_widget_area( 'home-hero' );
+
+ } // jpak_home_hero()
+
+
+ //-- LOAD FRAMEWORK --//
+ genesis();
