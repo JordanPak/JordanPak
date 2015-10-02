8fecc8d0cc143471810e2915007a502fcffbb39c
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-10-01 23:17:13 -0400

    Modify Commenting; init "Home Hero" Widget Area

--- functions.php
+++ functions.php
@@ -1,15 +1,11 @@
 <?php
 
-//------------------------//
-//  START GENESIS ENGINE  //
-//------------------------//
+//===========//
+//  GENESIS  //
+//===========//
+
+// Start Genesis Engine
 include_once( get_template_directory() . '/lib/init.php' );
-
-
-
-//-----------------------//
-//  EXTRA GENESIS STUFF  //
-//-----------------------//
 
 // HTML5 Markup Structure
 add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
@@ -17,20 +13,18 @@ add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list'
 // Viewport Meta Tag
 add_theme_support( 'genesis-responsive-viewport' );
 
-
-
-//----------------------//
-//  JORDANPAK INCLUDES  //
-//----------------------//
-
-// CHILD THEME STUFF
+// Definitions
 define( 'CHILD_THEME_NAME', 'JordanPak' );
 define( 'CHILD_THEME_URL', 'http://JordanPak.com/' );
 define( 'CHILD_THEME_VERSION', '1.0.0' );
 
 
 
-// ENQUEUE GLOBAL STYLES
+//===============//
+//  JPAK CUSTOM  //
+//===============//
+
+// GLOBAL STYLES
 add_action( 'wp_enqueue_scripts', 'jpak_global_styles' );
 function jpak_global_styles() {
 
@@ -39,3 +33,13 @@ function jpak_global_styles() {
     wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array(), CHILD_THEME_VERSION );
 
 } // jpak_global_styles()
+
+
+
+//  WIDGET AREAS  //
+
+genesis_register_sidebar( array(
+	'id'            => 'jimbo-social',
+	'name'          => __( 'Home Hero', 'jpak' ),
+	'description'   => __( 'For Text Widget', 'jpak' ),
+) );
