283f0442b8656f864b59cb97c0abe04159b042c2
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-10-08 18:48:17 -0400

    Init "Mini Hero"

--- functions.php
+++ functions.php
@@ -51,11 +51,25 @@ genesis_unregister_layout( 'sidebar-content-sidebar' ); // Sidebar-Content-Sideb
 add_action( 'wp_enqueue_scripts', 'jpak_global_styles' );
 function jpak_global_styles() {
 
-    // wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto:400,300,400italic,500,700', array(), CHILD_THEME_VERSION );
     wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto:400,300,400italic,500,700|Roboto+Condensed:400,700', array(), CHILD_THEME_VERSION );
     wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array(), CHILD_THEME_VERSION );
 
 } // jpak_global_styles()
+
+
+
+add_action( 'genesis_after_header', 'jpak_mini_hero' );
+/**
+* Mini Hero
+*
+* Echos the "Mini Hero" page header right below he site header
+*
+* @package JordanPak
+* @since 1.0.0
+*/
+function jpak_mini_hero() {
+
+} // jpak_mini_hero()
 
 
 
